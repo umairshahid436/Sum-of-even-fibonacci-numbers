e7aa9647b7740d35e2c19a0ba4772eff2dbd46cf
umaire313 <umairshahid436@gmail.com>
2017-10-19 21:40:36 +0500

    First Commit

---
+++ AssesmentTask/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssesmentTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Fibonacci_Iterative();
+        }
+
+        // Method For Fibonacci Series
+        public static void Fibonacci_Iterative()
+        {
+            int a = 0, b = 1, c = 0;
+            long sum=0; 
+            bool check = false;
+
+            // Loop will print Fibonacci Series
+
+            for (int i = 0; i < 4000000; i++)
+            {
+                c = a + b;
+                check = Program.IsEven(c);
+
+                // Condition that will check if number is even then add otherwise not
+                if (check)
+                {
+                    sum += c;
+                    check = false;
+                }
+                a = b;
+                b = c;
+            }
+            Console.WriteLine("Sum of even numbers in fibonacci series  {0}", sum);
+        }
+        //Check wether number is even
+        public static bool IsEven(int num)
+        {
+            return num % 2 == 0;
+        }
+    }
+}
